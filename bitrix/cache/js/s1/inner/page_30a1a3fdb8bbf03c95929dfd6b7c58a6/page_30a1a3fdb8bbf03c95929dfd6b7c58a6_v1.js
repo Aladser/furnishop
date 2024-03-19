d319ee318fe35b6928e6f6f398e9ac7381e1348f
Aladser <aladser@gmail.com>
2024-03-19 15:08:36 +0900

    up cache

--- bitrix/cache/js/s1/inner/page_30a1a3fdb8bbf03c95929dfd6b7c58a6/page_30a1a3fdb8bbf03c95929dfd6b7c58a6_v1.js
+++
@@ -1,7 +0,0 @@
-
-; /* Start:"a:4:{s:4:"full";s:73:"/bitrix/components/bitrix/menu/templates/tree/script.min.js?1710117505170";s:6:"source";s:55:"/bitrix/components/bitrix/menu/templates/tree/script.js";s:3:"min";s:59:"/bitrix/components/bitrix/menu/templates/tree/script.min.js";s:3:"map";s:59:"/bitrix/components/bitrix/menu/templates/tree/script.map.js";}"*/
-function OpenMenuNode(e){if(e.parentNode.className=="")e.parentNode.className="menu-close";else e.parentNode.className="";return false}
-/* End */
-;; /* /bitrix/components/bitrix/menu/templates/tree/script.min.js?1710117505170*/
-
-//# sourceMappingURL=page_30a1a3fdb8bbf03c95929dfd6b7c58a6.map.js
